2607ddecd70e28d139ed14da5c974f978bf23239
Jensen Wang <30664307+Cauchy-NY@users.noreply.github.com>
2019-06-11 22:00:46 +0800

    update: 重构为maven项目，更改jar包位置; add: 新增依赖

---
+++ A_GroupPurchaseManagementSystem/src/main/java/BankSystemImpl.java
@@ -0,0 +1,9 @@
+import assignment3.BankSystem;
+
+public class BankSystemImpl implements BankSystem {
+
+    @Override
+    public boolean transfer(String s, String s1, String s2, double v) {
+        return false;
+    }
+}
