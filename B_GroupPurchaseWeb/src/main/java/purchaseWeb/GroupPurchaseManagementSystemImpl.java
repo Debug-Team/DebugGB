c3e7505d0f7f016985ec625ab7ebe5dac1ab8aea
sunnybluez <1332901986@qq.com>
2019-06-12 11:11:03 +0800

    update
    update

--- B_GroupPurchaseWeb/src/main/java/purchaseWeb/GroupPurchaseManagementSystemImpl.java
+++ B_GroupPurchaseWeb/src/main/java/purchaseWeb/GroupPurchaseManagementSystemImpl.java
@@ -3,15 +3,26 @@ package purchaseWeb;
 import assignment3.GroupPurchaseItem;
 import assignment3.GroupPurchaseManagementSystem;
 
+import java.rmi.RemoteException;
 import java.util.List;
 
 public class GroupPurchaseManagementSystemImpl implements GroupPurchaseManagementSystem {
 
     public List<GroupPurchaseItem> listGroupPurchase() {
-        return PurchaseWebRMIHelper.getRemotePurchaseService().listGroupPurchase();
+        try {
+            return PurchaseWebRMIHelper.getRemotePurchaseService().listGroupPurchase();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public boolean submitPurchase(String s, String s1, String s2, String s3) {
-        return PurchaseWebRMIHelper.getRemotePurchaseService().submitPurchase(s, s1, s2, s3);
+        try {
+            return PurchaseWebRMIHelper.getRemotePurchaseService().submitPurchase(s, s1, s2, s3);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
