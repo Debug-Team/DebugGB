6d32cff7861ee55f652be869ee3ff308b9bddb97
161250181 <498690270@qq.com>
2019-06-14 21:37:02 +0800

    修改E的main

--- E_MyBankSystem/src/main/java/Main.java
+++ E_MyBankSystem/src/main/java/Main.java
@@ -1,9 +1,23 @@
-import corba.BankSystemCORBAServer;
+import zmq.BankSystemMQ;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-//        new BankSystemCORBAServer();
+        Thread threadForD = new Thread(){
+            @Override
+            public void run() {
+                BankSystemMQ.initMQServerForD();
+            }
+        };
+        threadForD.start();
+        Thread threadForA = new Thread(){
+            @Override
+            public void run() {
+//                System.out.println("a");
+                BankSystemMQ.initMQServerForA();
+            }
+        };
+        threadForA.start();
     }
 }
